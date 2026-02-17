f207e0fe257b3b37e29f1e2e8b4170b1396f7761
BashchukDanyil <basukdaniil1@gmail.com>
2026-02-17 16:08:18 +0200

    add function is_prime

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import utils
-def is_power_of_five(n):
-    """Повертає True, якщо число є степенем п'ятірки."""
-    if n < 1:
-        return False
-    while n % 5 == 0:
-        n //= 5
-    return n == 1
+
 print(utils.factorial(8))
+print(utils.is_prime(3)
 
--- utils.py
+++ utils.py
@@ -3,3 +3,15 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
+def is_prime(n):
+    """Повертає True, якщо число просте, інакше False."""
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True

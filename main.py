4e6040e9b745f142948538762078b197739aa19c
BashchukDanyil <basukdaniil1@gmail.com>
2026-02-17 15:42:47 +0200

    add function in main

---
+++ main.py
@@ -0,0 +1,8 @@
+def is_power_of_five(n):
+    """Повертає True, якщо число є степенем п'ятірки."""
+    if n < 1:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
+print(fibonacci(10))

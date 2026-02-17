0525080a15c5d7acfdf18c3ab4424d0fefeff400
BashchukDanyil <basukdaniil1@gmail.com>
2026-02-17 16:29:05 +0200

    add fibonacci function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import utils
 
 print(utils.factorial(8))
-print(utils.is_prime(3)
-
+print(utils.is_prime(3))
+print(utils.fibonacci(3))
--- utils.py
+++ utils.py
@@ -15,3 +15,18 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+
+def fibonacci(n):
+    """Повертає n-те число Фібоначчі (нумерація з 0)."""
+    if n < 0:
+        raise ValueError("n має бути невід'ємним")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
